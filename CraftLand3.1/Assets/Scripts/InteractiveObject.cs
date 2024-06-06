751f6ab37347a96058cb521d1201ca3b413d7c09
Vasek78951 <127154888+Vasek78951@users.noreply.github.com>
2024-06-06 23:37:09 +0200

    Delete CraftLand3.1 directory

--- CraftLand3.1/Assets/Scripts/InteractiveObject.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface InteractiveObject
-{
-    void Interact(); // Method to perform interaction
-    void StopInteraction(); // Method to stop interaction (if needed)
-}
